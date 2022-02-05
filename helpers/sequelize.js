6369429e5b5fde94afab8ba7b41ba41e2e35c8da
yudiz bhavin <yudiz_bhavin@Bhavins-MacBook-Pro.local>
2022-02-05 09:41:51 +0530

    for heroku

--- helpers/sequelize.js
+++ helpers/sequelize.js
@@ -1,16 +1,5 @@
 const Sequelize = require('sequelize')
 
-// const sequelize = new Sequelize('cart_demo', 'root', 'qwerty@123', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   logging: false,
-//   dialectOptions: { multipleStatements: true },
-//   pool: {
-//     max: 50,
-//     min: 5,
-//     handleDisconnects: true
-//   }
-// })
 const sequelize = new Sequelize(process.env.JAWSDB_URL || 'mysql://root:qwerty%40123@localhost:3306/cart_demo', {
   dialect: 'mysql',
   logging: false,
